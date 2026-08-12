6550be19e492226ddb9a99db33829dedde8924ef
Artemy Pozdnyakov <artem@typetype.org>
2026-08-13 00:42:45 +0300

    fix: add data to worldcup.sql
    Signed-off-by: Artemy Pozdnyakov <artem@typetype.org>

--- worldcup.sql
+++ worldcup.sql
@@ -83,6 +83,22 @@ ALTER SEQUENCE public.games_game_id_seq OWNED BY public.games.game_id;
 
 
 --
+-- Name: snus; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.snus (
+    year integer NOT NULL,
+    round character varying(255) NOT NULL,
+    winner character varying(255) NOT NULL,
+    opponent character varying(255) NOT NULL,
+    winner_goals integer NOT NULL,
+    opponent_goals integer NOT NULL
+);
+
+
+ALTER TABLE public.snus OWNER TO freecodecamp;
+
+--
 -- Name: teams; Type: TABLE; Schema: public; Owner: freecodecamp
 --
 
@@ -134,26 +150,120 @@ ALTER TABLE ONLY public.teams ALTER COLUMN team_id SET DEFAULT nextval('public.t
 -- Data for Name: games; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.games VALUES (1, 2018, 'Final', 11, 21, 4, 2);
+INSERT INTO public.games VALUES (2, 2018, 'Third Place', 22, 7, 2, 0);
+INSERT INTO public.games VALUES (3, 2018, 'Semi-Final', 21, 7, 2, 1);
+INSERT INTO public.games VALUES (4, 2018, 'Semi-Final', 11, 22, 1, 0);
+INSERT INTO public.games VALUES (5, 2018, 'Quarter-Final', 21, 17, 3, 2);
+INSERT INTO public.games VALUES (6, 2018, 'Quarter-Final', 7, 2, 2, 0);
+INSERT INTO public.games VALUES (7, 2018, 'Quarter-Final', 22, 19, 2, 1);
+INSERT INTO public.games VALUES (8, 2018, 'Quarter-Final', 11, 1, 2, 0);
+INSERT INTO public.games VALUES (9, 2018, 'Eighth-Final', 7, 5, 2, 1);
+INSERT INTO public.games VALUES (10, 2018, 'Eighth-Final', 2, 16, 1, 0);
+INSERT INTO public.games VALUES (11, 2018, 'Eighth-Final', 22, 12, 3, 2);
+INSERT INTO public.games VALUES (12, 2018, 'Eighth-Final', 19, 23, 2, 0);
+INSERT INTO public.games VALUES (13, 2018, 'Eighth-Final', 21, 13, 2, 1);
+INSERT INTO public.games VALUES (14, 2018, 'Eighth-Final', 17, 20, 2, 1);
+INSERT INTO public.games VALUES (15, 2018, 'Eighth-Final', 1, 4, 2, 1);
+INSERT INTO public.games VALUES (16, 2018, 'Eighth-Final', 11, 6, 4, 3);
+INSERT INTO public.games VALUES (17, 2014, 'Final', 3, 6, 1, 0);
+INSERT INTO public.games VALUES (18, 2014, 'Third Place', 18, 19, 3, 0);
+INSERT INTO public.games VALUES (19, 2014, 'Semi-Final', 6, 18, 1, 0);
+INSERT INTO public.games VALUES (20, 2014, 'Semi-Final', 3, 19, 7, 1);
+INSERT INTO public.games VALUES (21, 2014, 'Quarter-Final', 18, 24, 1, 0);
+INSERT INTO public.games VALUES (22, 2014, 'Quarter-Final', 6, 22, 1, 0);
+INSERT INTO public.games VALUES (23, 2014, 'Quarter-Final', 19, 5, 2, 1);
+INSERT INTO public.games VALUES (24, 2014, 'Quarter-Final', 3, 11, 1, 0);
+INSERT INTO public.games VALUES (25, 2014, 'Eighth-Final', 19, 9, 2, 1);
+INSERT INTO public.games VALUES (26, 2014, 'Eighth-Final', 5, 1, 2, 0);
+INSERT INTO public.games VALUES (27, 2014, 'Eighth-Final', 11, 15, 2, 0);
+INSERT INTO public.games VALUES (28, 2014, 'Eighth-Final', 3, 10, 2, 1);
+INSERT INTO public.games VALUES (29, 2014, 'Eighth-Final', 18, 23, 2, 1);
+INSERT INTO public.games VALUES (30, 2014, 'Eighth-Final', 24, 8, 2, 1);
+INSERT INTO public.games VALUES (31, 2014, 'Eighth-Final', 6, 16, 1, 0);
+INSERT INTO public.games VALUES (32, 2014, 'Eighth-Final', 22, 14, 2, 1);
+
+
+--
+-- Data for Name: snus; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.snus VALUES (2018, 'Final', 'France', 'Croatia', 4, 2);
+INSERT INTO public.snus VALUES (2018, 'Third Place', 'Belgium', 'England', 2, 0);
+INSERT INTO public.snus VALUES (2018, 'Semi-Final', 'Croatia', 'England', 2, 1);
+INSERT INTO public.snus VALUES (2018, 'Semi-Final', 'France', 'Belgium', 1, 0);
+INSERT INTO public.snus VALUES (2018, 'Quarter-Final', 'Croatia', 'Russia', 3, 2);
+INSERT INTO public.snus VALUES (2018, 'Quarter-Final', 'England', 'Sweden', 2, 0);
+INSERT INTO public.snus VALUES (2018, 'Quarter-Final', 'Belgium', 'Brazil', 2, 1);
+INSERT INTO public.snus VALUES (2018, 'Quarter-Final', 'France', 'Uruguay', 2, 0);
+INSERT INTO public.snus VALUES (2018, 'Eighth-Final', 'England', 'Colombia', 2, 1);
+INSERT INTO public.snus VALUES (2018, 'Eighth-Final', 'Sweden', 'Switzerland', 1, 0);
+INSERT INTO public.snus VALUES (2018, 'Eighth-Final', 'Belgium', 'Japan', 3, 2);
+INSERT INTO public.snus VALUES (2018, 'Eighth-Final', 'Brazil', 'Mexico', 2, 0);
+INSERT INTO public.snus VALUES (2018, 'Eighth-Final', 'Croatia', 'Denmark', 2, 1);
+INSERT INTO public.snus VALUES (2018, 'Eighth-Final', 'Russia', 'Spain', 2, 1);
+INSERT INTO public.snus VALUES (2018, 'Eighth-Final', 'Uruguay', 'Portugal', 2, 1);
+INSERT INTO public.snus VALUES (2018, 'Eighth-Final', 'France', 'Argentina', 4, 3);
+INSERT INTO public.snus VALUES (2014, 'Final', 'Germany', 'Argentina', 1, 0);
+INSERT INTO public.snus VALUES (2014, 'Third Place', 'Netherlands', 'Brazil', 3, 0);
+INSERT INTO public.snus VALUES (2014, 'Semi-Final', 'Argentina', 'Netherlands', 1, 0);
+INSERT INTO public.snus VALUES (2014, 'Semi-Final', 'Germany', 'Brazil', 7, 1);
+INSERT INTO public.snus VALUES (2014, 'Quarter-Final', 'Netherlands', 'Costa Rica', 1, 0);
+INSERT INTO public.snus VALUES (2014, 'Quarter-Final', 'Argentina', 'Belgium', 1, 0);
+INSERT INTO public.snus VALUES (2014, 'Quarter-Final', 'Brazil', 'Colombia', 2, 1);
+INSERT INTO public.snus VALUES (2014, 'Quarter-Final', 'Germany', 'France', 1, 0);
+INSERT INTO public.snus VALUES (2014, 'Eighth-Final', 'Brazil', 'Chile', 2, 1);
+INSERT INTO public.snus VALUES (2014, 'Eighth-Final', 'Colombia', 'Uruguay', 2, 0);
+INSERT INTO public.snus VALUES (2014, 'Eighth-Final', 'France', 'Nigeria', 2, 0);
+INSERT INTO public.snus VALUES (2014, 'Eighth-Final', 'Germany', 'Algeria', 2, 1);
+INSERT INTO public.snus VALUES (2014, 'Eighth-Final', 'Netherlands', 'Mexico', 2, 1);
+INSERT INTO public.snus VALUES (2014, 'Eighth-Final', 'Costa Rica', 'Greece', 2, 1);
+INSERT INTO public.snus VALUES (2014, 'Eighth-Final', 'Argentina', 'Switzerland', 1, 0);
+INSERT INTO public.snus VALUES (2014, 'Eighth-Final', 'Belgium', 'United States', 2, 1);
 
 
 --
 -- Data for Name: teams; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.teams VALUES (1, 'Uruguay');
+INSERT INTO public.teams VALUES (2, 'Sweden');
+INSERT INTO public.teams VALUES (3, 'Germany');
+INSERT INTO public.teams VALUES (4, 'Portugal');
+INSERT INTO public.teams VALUES (5, 'Colombia');
+INSERT INTO public.teams VALUES (6, 'Argentina');
+INSERT INTO public.teams VALUES (7, 'England');
+INSERT INTO public.teams VALUES (8, 'Greece');
+INSERT INTO public.teams VALUES (9, 'Chile');
+INSERT INTO public.teams VALUES (10, 'Algeria');
+INSERT INTO public.teams VALUES (11, 'France');
+INSERT INTO public.teams VALUES (12, 'Japan');
+INSERT INTO public.teams VALUES (13, 'Denmark');
+INSERT INTO public.teams VALUES (14, 'United States');
+INSERT INTO public.teams VALUES (15, 'Nigeria');
+INSERT INTO public.teams VALUES (16, 'Switzerland');
+INSERT INTO public.teams VALUES (17, 'Russia');
+INSERT INTO public.teams VALUES (18, 'Netherlands');
+INSERT INTO public.teams VALUES (19, 'Brazil');
+INSERT INTO public.teams VALUES (20, 'Spain');
+INSERT INTO public.teams VALUES (21, 'Croatia');
+INSERT INTO public.teams VALUES (22, 'Belgium');
+INSERT INTO public.teams VALUES (23, 'Mexico');
+INSERT INTO public.teams VALUES (24, 'Costa Rica');
 
 
 --
 -- Name: games_game_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.games_game_id_seq', 1, false);
+SELECT pg_catalog.setval('public.games_game_id_seq', 32, true);
 
 
 --
 -- Name: teams_team_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.teams_team_id_seq', 1, false);
+SELECT pg_catalog.setval('public.teams_team_id_seq', 24, true);
 
 
 --
